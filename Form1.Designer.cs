cc6937c15c0b8d5e53595fcdcc2f7a1ac9af3307
Ivar Virusiim <Ivar.Virusiim@gmail.com>
2019-06-23 12:06:31 +0300

    First real commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace AutoFisher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scanButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // scanButton
+            // 
+            this.scanButton.Location = new System.Drawing.Point(12, 43);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(131, 48);
+            this.scanButton.TabIndex = 0;
+            this.scanButton.Text = "Scan";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 9);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(55, 13);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Starting....";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(149, 43);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(105, 48);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 103);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.scanButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using AutoFisher.Properties;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoFisher
+{
+    public partial class Form1 : Form
+    {
+        int xStart = 960;
+        int xEnd = 1320;
+        int yStart = 550;
+        int yEnd = 800;
+        volatile static bool running = false;
+        volatile static bool stopRequested = false;
+        volatile static bool breakTime = false;
+        int numRunning = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsInCapture(Bitmap searchFor, Bitmap searchIn, out Tuple<int, int> point)
+        {
+            point = null;
+            for (int x = xStart; x < xEnd; x++)
+            {
+                for (int y = yStart; y < yEnd; y++)
+                {
+                    bool invalid = false;
+                    int k = x, l = y;
+                    for (int a = 0; a < searchFor.Width; a++)
+                    {
+                        l = y;
+                        for (int b = 0; b < searchFor.Height; b++)
+                        {
+                            if (breakTime)// Other thread found something
+                                return false;
+
+                            if (searchFor.GetPixel(a, b) != searchIn.GetPixel(k, l))
+                            {
+                                invalid = true;
+                                point = null;
+                                break;
+                            }
+                            else
+                            {
+                                if (point == null)
+                                    point = new Tuple<int, int>(k, l);
+                                l++;
+                            }
+                        }
+                        if (invalid)
+                            break;
+                        else
+                            k++;
+                    }
+                    if (!invalid)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        Bitmap[] pics = { Resources.clickOr1, Resources.clickOr2, Resources.clickOr3, Resources.clickOr4, Resources.clickOr5 };
+        static ConcurrentDictionary<int, int> stats = new ConcurrentDictionary<int, int>();
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {
+                var mainThread = new Thread(() =>
+                {
+                    while (!stopRequested)
+                    {
+                        setStatus("In progress");
+                        int outerI = 0;
+                        foreach (Bitmap pix in pics)
+                        {
+
+                            var task = new Task(() =>
+                            {
+                                var i = Interlocked.Increment(ref outerI);
+                                var pic = pics[i - 1];
+                                Debug.WriteLine("Thread starting: " + i);
+
+                                try
+                                {
+                                    Bitmap screenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+                                    Graphics g = Graphics.FromImage(screenCapture);
+
+                                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                                 Screen.PrimaryScreen.Bounds.Y,
+                                                 0, 0,
+                                                 screenCapture.Size,
+                                                 CopyPixelOperation.SourceCopy);
+
+
+
+                                    bool isInCapture = IsInCapture(pic, screenCapture, out Tuple<int, int> point);
+                                    if (isInCapture)
+                                    {
+                                        breakTime = true;
+                                        setStatus($"Pic: {i} point: {point?.Item1};{point?.Item2}");
+                                        LeftMouseClick(point.Item1, point.Item2);
+                                        Thread.Sleep(100);
+                                        SetCursorPos(0, 0);
+                                        stats.AddOrUpdate(i, 1, (a, b) => Interlocked.Increment(ref b));
+
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                    Debug.WriteLine("Problem with Thread " + i);
+                                }
+
+                                Interlocked.Decrement(ref numRunning);
+                                Debug.WriteLine("Thread stopping: " + i);
+                            });
+                            task.Start();
+                            Interlocked.Increment(ref numRunning);
+                        }
+
+                        Thread.Sleep(500);
+
+                        while (numRunning != 0)
+                        {
+                            Debug.WriteLine("Threads still running: " + numRunning);
+                            Thread.Sleep(100);
+                        }
+                        breakTime = false;
+                    }
+                    stopRequested = false;
+                    running = false;
+                    setStatus("Done");
+                    foreach (var kvp in stats)
+                        Debug.WriteLine("Picture = {0}, Count = {1}", kvp.Key, kvp.Value);
+
+                });
+                mainThread.SetApartmentState(ApartmentState.STA);
+                mainThread.IsBackground = true;
+                mainThread.Start();
+                running = true;
+            }
+        }
+
+        private void setStatus(String status)
+        {
+            Action update = () => statusLabel.Text = status;
+            if (statusLabel.InvokeRequired)
+                statusLabel.Invoke(update);
+            else
+                update();
+        }
+
+        //This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (!stopRequested)
+                stopRequested = true;
+        }
+    }
+
+
+}
